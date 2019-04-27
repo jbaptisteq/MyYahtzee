b3bd3e700dc85eeac5dcfbdae5abd86a4eacced2
jbaptiste <jb.queralt@gmail.com>
2019-04-27 11:54:10 +0200

    fix : remove useless use

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -9,8 +9,6 @@ use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\NotBlank;
-use Symfony\Component\Validator\Constraints\Length;
 
 class RegistrationFormType extends AbstractType
 {
