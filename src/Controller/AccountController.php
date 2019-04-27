3b565dec00460e23c9f03828995aef635965d80d
jbaptiste <jb.queralt@gmail.com>
2019-04-27 11:53:48 +0200

    issue #7 Edit Email

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -5,26 +5,48 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use App\Form\EditEmailFormType;
 
 class AccountController extends AbstractController
 {
     /**
-     * @Route("/account", name="app_account")
-     */
+    * @Route("/account", name="app_account")
+    */
     public function account(): Response
     {
 
         return $this->render('pages/account.html.twig');
     }
 
-    public function changeEmail()
+    /**
+    * @Route("/account/editEmail", name="app_editEmail")
+    */
+    public function editEmail(Request $request)
     {
+        $user = $this->getUser();
+        $form = $this->createForm(EditEmailFormType::class, $user);
+        $form->handleRequest($request);
 
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $user->setEmail($form->get('email')->getData());
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_account');
+        }
+
+        return $this->render('pages/editEmail.html.twig', [
+            'editEmailForm' => $form->createView(),
+        ]);
     }
 
     public function changePassword()
     {
-        
+
     }
 
 }
